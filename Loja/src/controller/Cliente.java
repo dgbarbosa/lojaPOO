70985ffc0755ca81a41dc450087e87692c9082ea
Renan <mirandarenan.e@gmail.com>
2018-06-09 18:15:07 -0300

    Update Cliente

--- Loja/src/controller/Cliente.java
+++ Loja/src/controller/Cliente.java
@@ -1,20 +1,20 @@
 package controller;
 
 public class Cliente {
-	private int prod_id;
+	private int cliente_id;
 	private String nome;
 	private int cpf;
 	private String endereco;
 	private int clienteUsuario_id;
 	public Cliente(int id, String nome, int cpf, String endereco, int user_id) {
-		this.prod_id = id;
+		this.cliente_id = id;
 		this.nome = nome;
 		this.cpf = cpf;
 		this.endereco = endereco;
 		this.clienteUsuario_id = user_id;		
 	}
-	public int getProd_id() {
-		return prod_id;
+	public int getCliente_id() {
+		return cliente_id;
 	}
 	public String getNome() {
 		return nome;
--- Loja/src/model/ClienteDao.java
+++ Loja/src/model/ClienteDao.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import controller.Cliente;
@@ -48,6 +49,45 @@ public class ClienteDao {
 		ps.setInt(1, id);
 		ps.execute();
 		ps.close();
+	}
+	public void alterarNomeCliente(int id, String nome) throws SQLException {
+		String sql = "UPDATE Cliente SET nome=? WHERE user_id=?;";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, nome);
+		ps.setInt(2, id);
+		ps.execute();
+		ps.close();
+	}
+	public Cliente listarUmCliente(int id) throws SQLException {
+		String sql = "SELECT * from Cliente WHERE user_id=?;";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setInt(1, id);
+		ResultSet rs = ps.executeQuery();
+		ps.close();
+		return new Cliente(rs.getInt("cliente_id"),
+				rs.getString("nome"),
+				rs.getInt("cpf"),
+				rs.getString("endereco"),
+				rs.getInt("clienteUsuario_id")
+				);
+	}
+	
+	public void listar() throws SQLException {
+		String sql = "SELECT * from Cliente;";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()) {
+			System.out.println(rs.getObject("id"));
+			System.out.println(rs.getObject("nome"));
+			System.out.println(rs.getObject("cpf"));
+			System.out.println(rs.getObject("endereco"));
+			System.out.println(rs.getObject("clienteUsuario_id"));
+		}
+		ps.close();
+	}
+	
+	public void fechar() throws SQLException {
+		conn.close();
 	}
 	
 }
